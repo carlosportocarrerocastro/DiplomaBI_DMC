a5ef2158fe6718ead376d55c5eea44699e9558fb
carlosportocarrerocastro <carlos.portocarrero.castro@gmail.com>
2026-06-01 20:29:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,6 @@ st.title("Proyecto Final Diploma BI")
 st.sidebar.title("Parámetros")
 
 st.image("Python_logo.png",width=500)
-st.sidebar.image("DMC.png",width=300)
+st.sidebar.image("DMC.png",width=100)
+
+st.write("Elaborado por Carlos Portocarrero")
